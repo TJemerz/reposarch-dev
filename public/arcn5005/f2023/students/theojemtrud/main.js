1a46ca50e003b7c123c69fa1213ab512763afb2c
Theo Jemtrud <tjemtrud@me.com>
2023-11-14 18:06:29 -0500

    Three js assignment

--- public/arcn5005/f2023/students/theojemtrud/main.js
+++ public/arcn5005/f2023/students/theojemtrud/main.js
@@ -28,23 +28,51 @@ document.body.appendChild(renderer.domElement);
 //axes.renderOrder = 1;
 //scene.add(axes);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.SphereGeometry(0.5, 32, 16);
 
-const yellowMaterial = new THREE.MeshLambertMaterial({ color: 0xffff00 });
-const blueMaterial = new THREE.MeshLambertMaterial({ color: 0x0000ff });
-const greenMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+const yellowMaterial = new THREE.MeshBasicMaterial({ color: 0xffd97d });
+const blueMaterial = new THREE.MeshBasicMaterial({ color: 0xfb4242 });
+const redMaterial = new THREE.MeshBasicMaterial({ color: 0x63250e });
+const greenMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const AMaterial = new THREE.MeshBasicMaterial({ color: 0xffd97d });
+const BMaterial = new THREE.MeshBasicMaterial({ color: 0xfb4242 });
+const CMaterial = new THREE.MeshBasicMaterial({ color: 0x63250e });
+const DMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
 
-const yellowCube = new THREE.Mesh(geometry, yellowMaterial);
-const blueCube = new THREE.Mesh(geometry, blueMaterial);
-const greenCube = new THREE.Mesh(geometry, greenMaterial);
+const yellowSphere = new THREE.Mesh(geometry, yellowMaterial);
+const blueSphere = new THREE.Mesh(geometry, blueMaterial);
+const redSphere = new THREE.Mesh(geometry, redMaterial);
+const greenSphere = new THREE.Mesh(geometry, greenMaterial);
+const ASphere = new THREE.Mesh(geometry, yellowMaterial);
+const BSphere = new THREE.Mesh(geometry, blueMaterial);
+const CSphere = new THREE.Mesh(geometry, redMaterial);
+const DSphere = new THREE.Mesh(geometry, greenMaterial);
 
-yellowCube.position.z = -3;
-blueCube.position.x = -3;
-greenCube.position.z = 3;
+yellowSphere.position.z = -3;
+blueSphere.position.x = -3;
+redSphere.position.x = 3;
+greenSphere.position.z = 3;
+yellowSphere.position.y = -3;
+blueSphere.position.y = -3;
+redSphere.position.y = -3;
+greenSphere.position.y = -3;
+ASphere.position.z = 3;
+BSphere.position.x = 3;
+CSphere.position.x = -3;
+DSphere.position.z = -3;
+ASphere.position.y = 5;
+BSphere.position.y = 5;
+CSphere.position.y = 5;
+DSphere.position.y = 5;
 
-scene.add(yellowCube);
-scene.add(blueCube);
-scene.add(greenCube);
+scene.add(yellowSphere);
+scene.add(blueSphere);
+scene.add(redSphere);
+scene.add(greenSphere);
+scene.add(ASphere);
+scene.add(BSphere);
+scene.add(CSphere);
+scene.add(DSphere);
 
 
 //const loader = new THREE.GLTFLoader();
@@ -117,14 +145,17 @@ function animate() {
 
   if (mesh) mesh.rotation.y += 0.005;
 
-  yellowCube.rotation.x += 0.001;
-  yellowCube.rotation.y += 0.001;
+  yellowSphere.rotation.x += 0.001;
+  yellowSphere.rotation.y += 0.001;
 
-  blueCube.rotation.x += 0.001;
-  blueCube.rotation.y -= 0.001;
+  blueSphere.rotation.x += 0.001;
+  blueSphere.rotation.y -= 0.001;
 
-  greenCube.rotation.x += 0.001;
-  greenCube.rotation.y -= 0.001;
+  redSphere.rotation.x -= 0.001;
+  redSphere.rotation.y -= 0.001;
+
+  greenSphere.rotation.x += 0.001;
+  greenSphere.rotation.y -= 0.001;
 
   renderer.render(scene, camera);
 }
@@ -141,6 +172,48 @@ const loader = new THREE.TextureLoader();
       scene.background = texture;
 
     });
+
+    const fontLoader = new THREE.FontLoader();
+
+    function createText(text, z = 0, textColor = "0x000000", size = 0.5) {
+      const textValue = text;
+      const textSize = size;
+      fontLoader.load("./fonts/helvetiker_regular.typeface.json", function (font) {
+    
+        const textGeo = new THREE.TextGeometry(textValue, {
+          font: font,
+          size: textSize,
+          height: 0.1,
+          curveSegments: 4,
+          bevelEnabled: true,
+          bevelThickness: 0.05,
+          bevelSize: 0.0,
+          bevelOffset: 0,
+          bevelSegments: 5,
+    
+        });
+    
+        const color = new THREE.Color();
+
+        color.setHex(textColor);
+        const textMaterial = new THREE.MeshBasicMaterial({ color: color });
+        const text = new THREE.Mesh(textGeo, textMaterial);
+    
+        text.position.x = 7;
+        text.position.y = z;
+        text.position.z = 0;
+      
+        scene.add(text);
+    
+      });
+    
+    }
+    
+    createText("Theo Jemtrud", 5, "0X1f400a");
+    createText("- From Montreal, Qc", 3, "0X000000");
+    createText("- Favourite movie: The Big Lebowski", 2, "0X000000");
+    createText("- Favourite color is blue", 1, "0X000000");
+    createText("- Favourite drink Negroni", 0, "0X000000");
 
 //Adjust the viewport to the size of the browser
 window.addEventListener("resize", () => {
